af48ad727a37a72e8d85621c810eead591f30759
Estudiante <Estudiante@DESKTOP-TITVKIH>
2025-02-18 14:30:08 -0300

    Agregar archivos de proyecto.

---
+++ ConsoleApp16/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace ConsoleApp16
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] height = { 1, 8, 6, 2, 5, 4, 8, 3, 7 };
+            Console.WriteLine(MaxArea(height));
+            Console.ReadLine();
+        }
+
+        public static int MaxArea(int[] height)
+        {
+            int inicio = 0;
+            int final = height.Length - 1;
+            int TotalMaximo = 0;
+
+            while (inicio < final)
+            {
+                int total = Math.Min(height[inicio], height[final]) * (final - inicio);
+                Console.WriteLine(total);
+
+                if (total > TotalMaximo)
+                {
+                    TotalMaximo = total;
+                }
+
+                // Mover el puntero que tenga menor altura
+                if (height[inicio] < height[final])
+                {
+                    inicio++;
+                }
+                else
+                {
+                    final--;
+                }
+            }
+
+            return TotalMaximo;
+        }
+    }
+}
---
+++ ConsoleApp16/Properties/AssemblyInfo.cs
@@ -0,0 +1,13 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Si establece ComVisible en false, los tipos de este ensamblado no estarán visibles 
+// para los componentes COM.  Si es necesario obtener acceso a un tipo en este ensamblado desde 
+// COM, establezca el atributo ComVisible en true en este tipo.
+[assembly: ComVisible(false)]
+
+// El siguiente GUID sirve como id. de typelib si este proyecto se expone a COM.
+[assembly: Guid("8a83c5b2-fea9-4a2d-8077-7613aee60eca")]
